3ce572e1cf1a9c7409e9cdb8dd42a15be3c52e20
Natnael Makonnen <natmak2011@gmail.com>
2023-03-24 12:05:11 +0300

    Initial commit SpringBoot project created with dependencies

---
+++ src/main/java/com/paps/paps/PapsApplication.java
@@ -0,0 +1,13 @@
+package com.paps.paps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapsApplication.class, args);
+	}
+
+}
