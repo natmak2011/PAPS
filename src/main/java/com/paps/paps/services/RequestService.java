249f69fdcd73ce300634f520ef3425359c02e602
Natnael Makonnen <natmak2011@gmail.com>
2023-03-25 12:42:09 +0300

    TDD for Date of Birth, null and under age (<17)!

--- src/main/java/com/paps/paps/services/RequestService.java
+++ src/main/java/com/paps/paps/services/RequestService.java
@@ -15,7 +15,16 @@ public class RequestService {
 
     public Request createRequest(Request request){
 
-        AgeCalculator.calculateDifference(request.getDate_of_birth(), LocalDate.now());
+        Integer age = AgeCalculator.calculateDifference(request.getDate_of_birth(), LocalDate.now());
+        if (age== null){
+
+            throw new RuntimeException("Date of Birth Can NOT be Null");
+        }
+
+        if (age < 18){
+
+            throw new RuntimeException("Your are Under Age, Use proxy.");
+        }
        return requestRepository.save(request);
     }
 }
