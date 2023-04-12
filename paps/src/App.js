28cbdaf6194fafb750ca29cc4eec2547db212f15
Natnael Makonnen <natmak2011@gmail.com>
2023-04-12 23:29:17 +0300

    Request Passport web

--- paps/src/App.js
+++ paps/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from "./Header";
 import {Switch} from "@mui/material";
 import CreateRequest from "./CreateRequest";
+import CreateWatchlist  from "./CreateWatchlist";
 import {Route, Routes} from "react-router-dom";
 
 const App = () => {
@@ -13,15 +14,12 @@ const App = () => {
 
             <Routes>
 
-                <Route path='/' element={<CreateRequest/>}/>
+                <Route path="/" element={<CreateRequest/>}/>
+                <Route path="/watchlist" element={<CreateWatchlist/>}/>
 
 
             </Routes>
         </>
-
-
-
-
 
 
     )
